57b86eff0e888b2687b5adef802b3ed3800af55b
rezakali <rezakali1993@gmail.com>
2022-05-29 00:25:37 +0530

    Links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
     return ( 
         <div className="footerWrapper d-flex align-items-center justify-content-center">
-            <a href="" className="social-btn"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            <a href="" className="social-btn"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="" className="social-btn"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            <a href="" className="social-btn"><i class="fa fa-github" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/rezakali" target="_blank" className="social-btn"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="https://www.facebook.com/rezakali" target="_blank" className="social-btn"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/rezakali/" target="_blank" className="social-btn"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+            <a href="https://github.com/rezakali/" target="_blank" className="social-btn"><i class="fa fa-github" aria-hidden="true"></i></a>
         </div>
      );
 }
