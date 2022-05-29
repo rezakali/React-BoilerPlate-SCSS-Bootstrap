7139c6b2cb132966ca78031583d80af89f6f02a2
rezakali <rezakali1993@gmail.com>
2022-05-29 16:17:46 +0530

    Cleaned Up

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,14 +4,7 @@ const Content = () => {
     return ( 
         <div className="contentWrapper">
             <Container>
-                <h1>Fun facts about React</h1>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 100K stars on GitHub</li>
-                    <li>Is maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
+                <h1>Hey there !!!</h1>
             </Container>
         </div>
      );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,10 @@
-import logo from "../images/reactjs-icon.png";
 import { Container } from "react-bootstrap";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <Container>
-        <div className="row align-items-center py-3">
-          <div className="col-6">
-            <div className="logo-wrapper d-flex align-items-center">
-              <img src={logo} alt="Logo" />
-              <h3>ReactFacts</h3>
-            </div>
-          </div>
-          <div className="col-6 text-end">
-            <div className="heading">
-              <h4>React Course - Project 1</h4>
-            </div>
-          </div>
-        </div>
+        <h3>React SCSS Bootstrap BoilerPlate</h3>
       </Container>
     </div>
   );
